d1fb59e37cd75ffdc84098bad6708e029433cb1c
MChavezSaa <matroxmega@gmail.com>
2019-07-07 00:39:12 -0400

    se arreglo errores sintaxicos en c-bolsa ts

--- src/app/cliente/c-bolsa/c-bolsa.component.ts
+++ src/app/cliente/c-bolsa/c-bolsa.component.ts
@@ -9,30 +9,31 @@ import { IBolsa } from './IBolsa';
 export class CBolsaComponent implements OnInit {
 
   constructor() { }
+  bolsa: IBolsa[] = [
+    {
+
+     nombre: 'Torta fondant',
+     descripcion: 'Torta fondant',
+     cantidad: 10,
+     precio: 18000,
+     url: 'http://annaspasteleria.com/wp-content/uploads/2018/10/29-2483-post/DSC_0143web.jpg'
+    },
+    {
+     nombre: 'Torta de manjar',
+     descripcion: 'Torta de manjar',
+     cantidad: 15,
+     precio: 15000,
+     url: 'https://www.gourmet.cl/wp-content/uploads/2017/05/torta-choc-manjar-editada.jpg'
+    },
+    {
+     nombre: 'Torta mil hojas',
+     descripcion: 'Torta mil hojas',
+     cantidad: 15,
+     precio: 15000,
+     url: 'https://www.gourmet.cl/wp-content/uploads/2017/02/16.jpg'
+    }
+  ];
 
   ngOnInit() {
   }
-  bolsa: IBolsa[] =[
-    {
-     "nombre":"Torta fondant",
-     "descripcion": "Torta fondant",
-     "cantidad": 10,
-     "precio": 18000,
-     "url":"http://annaspasteleria.com/wp-content/uploads/2018/10/29-2483-post/DSC_0143web.jpg"
-    },
-    {
-     "nombre":"Torta de manjar",
-     "descripcion": "Torta de manjar",
-     "cantidad": 15,
-     "precio": 15000,
-     "url":"https://www.gourmet.cl/wp-content/uploads/2017/05/torta-choc-manjar-editada.jpg"
-    },
-    {
-     "nombre":"Torta mil hojas",
-     "descripcion": "Torta mil hojas",
-     "cantidad": 15,
-     "precio": 15000,
-     "url":"https://www.gourmet.cl/wp-content/uploads/2017/02/16.jpg"
-    }
-  ];
 }
