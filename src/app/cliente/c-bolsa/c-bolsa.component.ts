9f566fe792c14dd62a36afcde02e6f08345358ad
MChavezSaa <matroxmega@gmail.com>
2019-07-07 00:06:20 -0400

    bolsa terminada (ngfor + array)

--- src/app/cliente/c-bolsa/c-bolsa.component.ts
+++ src/app/cliente/c-bolsa/c-bolsa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IBolsa } from './cliente/c-bolsa/IBolsa';
 
 @Component({
   selector: 'app-c-bolsa',
@@ -11,5 +12,27 @@ export class CBolsaComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  bolsa: IBolsa[] =[
+    {
+     "nombre":"Torta fondant",
+     "descripcion": "Torta fondant",
+     "cantidad": 10,
+     "precio": 18000,
+     "url":"http://annaspasteleria.com/wp-content/uploads/2018/10/29-2483-post/DSC_0143web.jpg"
+    },
+    {
+     "nombre":"Torta de manjar",
+     "descripcion": "Torta de manjar",
+     "cantidad": 15,
+     "precio": 15000,
+     "url":"https://www.gourmet.cl/wp-content/uploads/2017/05/torta-choc-manjar-editada.jpg"
+    },
+    {
+     "nombre":"Torta mil hojas",
+     "descripcion": "Torta mil hojas",
+     "cantidad": 15,
+     "precio": 15000,
+     "url":"https://www.gourmet.cl/wp-content/uploads/2017/02/16.jpg"
+    }
+  ];
 }
