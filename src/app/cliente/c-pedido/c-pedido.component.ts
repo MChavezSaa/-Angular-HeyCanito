5e6007b221829021527c6114cb2ef4ac59a8ceba
MiniChii <f.p.chii@gmail.coom>
2019-07-27 02:10:15 -0400

    Update c-pedido.component.ts
    Se agrega el detalle del pedido a la bdd

--- src/app/cliente/c-pedido/c-pedido.component.ts
+++ src/app/cliente/c-pedido/c-pedido.component.ts
@@ -3,8 +3,10 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ProductoService } from 'src/app/servicio/producto.service';
-import { IPedido } from '../../administrador/pedido/IPedido';
+import { IPedido } from '../../IPedido';
 import { Iproduct } from 'src/app/producto';
+import { log } from 'util';
+import { IDetalle } from 'src/app/IDetalle';
 
 @Component({
   selector: 'app-c-pedido',
@@ -16,30 +18,34 @@ export class CPedidoComponent implements OnInit {
   formProduct: FormGroup;
 
   bolsa: Iproduct[];
+  pedido: IPedido
 
-  constructor(private router: Router,
+  constructor(
+    private router: Router,
     public productService: ProductoService,
-    private formBuilder: FormBuilder) {
+    
+    private formBuilder: FormBuilder )
+      { /**para el formulario */
       const dateLength = 10;
       const today = new Date().toISOString().substring(0, dateLength);
-    this.formProduct = this.formBuilder.group({
-      nombre:['',[Validators.required]],
-      rut:['',[Validators.required,Validators.maxLength(12)]],
-      telefono:['',[Validators.required,Validators.pattern('[1-9]{1}[0-9]{8}')]],
-      direccion: ['', ],
-      mail:['',[Validators.email]],
-      modo_entrega: ['', [Validators.required]],
-      fecha_inicio: [today,],
-      fecha_entrega: ['',],
-      fecha_pago: ['',],
-      valor_total: [0,],
-      metodo_pago: ['', [Validators.required]],
-      estado: ['bolsa',], 
-    });
 
-    this.bolsa=this.productService.bolsa;
+      this.formProduct = this.formBuilder.group({
+        nombre:['',[Validators.required]],
+        rut:['',[Validators.required,Validators.maxLength(12)]],
+        telefono:['',[Validators.required,Validators.pattern('[1-9]{1}[0-9]{8}')]],
+        direccion: ['', ],
+        mail:['',[Validators.email]],
+        modo_entrega: ['', [Validators.required]],
+        fecha_inicio: [today,],
+        fecha_entrega: ['',],
+        fecha_pago: ['',],
+        valor_total: [0,],
+        metodo_pago: ['', [Validators.required]],
+        estado: ['Bolsa de compras',], 
+      });
 
-  }
+      //this.bolsa=this.productService.bolsa;
+      }
 
  
 
@@ -47,9 +53,8 @@ export class CPedidoComponent implements OnInit {
   ngOnInit() {
 
   }
-
+  /* Suma el precio de los productos en la bolsa */
   calcularTotal():number{
-
     let total=0;
     if(this.bolsa){
       for (let prod of this.bolsa) {
@@ -57,27 +62,45 @@ export class CPedidoComponent implements OnInit {
       }
     }else{
       this.bolsa= this.productService.bolsa;
-      
     }
     return total;
   }
+
+  /* genera el pedido */
   guardarPedido(pedido: IPedido) {
     console.log(pedido);    //agregar cosas
+    /* hace la peticion a la bdd */
+    this.productService.savePedido(pedido).subscribe((res: any[]) => {
+      this.productService.getPedido(res.datos.insertId).subscribe((res2: any[])=>{
+        this.productService.pedido=res2[0];
+        
+        },err => console.log(err))
+        /* agregar cada detalle */
+        if(this.bolsa){
+          let cantidad=1; 
+          for (let prod of this.bolsa) {
+            let detalle:IDetalle = {
+              id_pedido: res.datos.insertId,
+              id_producto: prod.id,
+              cantidad_producto: cantidad
+            }
+            console.log(detalle);
+            this.productService.agregarDetalle(detalle)
+              .subscribe((res3:any[])=>{console.log(res3);},err => console.log(err));     
+          }
+        }
+      },err => console.log(err));
     
-    this.productService.savePedido(pedido).subscribe(() => {
-      return this.productService.getProducts().subscribe((res: any[]) => {
-        this.productService.products = res;
-      },
-        err => console.log(err));
-    })
+      
+      console.log(this.productService.pedido);
+      
+      
+
   }
   /**Guarda el formulario del pedido */
   saveData() {
 
-    this.bolsa=this.productService.bolsa;
-    console.log("--FORM_PRODUCT-antes-");
-    console.log(this.formProduct.value);
-   
+    this.bolsa=this.productService.bolsa;   
     this.formProduct.setValue({
       nombre: this.formProduct.get('nombre').value,
       rut:this.formProduct.get('rut').value,
@@ -90,13 +113,11 @@ export class CPedidoComponent implements OnInit {
       fecha_pago: this.formProduct.get('fecha_pago').value,
       valor_total: this.calcularTotal(),
       metodo_pago: this.formProduct.get('metodo_pago').value,
-      estado: 'generado', 
+      estado: 'Generado', 
     });
-    console.log("--FORM_PRODUCT-despues-");
-    console.log(this.formProduct.value);
     this.guardarPedido(this.formProduct.value);
     //redireccionamiento a home
-
-    this.router.navigate(['home']);
+    //this.router.navigate(['home']);
+    
   }
 }
