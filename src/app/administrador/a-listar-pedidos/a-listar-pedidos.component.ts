0a357f8c99a05812e8b11e15e3bd54d3fc888536
MiniChii <f.p.chii@gmail.coom>
2019-07-28 23:14:11 -0400

    listar pedidos, conn campos deshabilitados

--- src/app/administrador/a-listar-pedidos/a-listar-pedidos.component.ts
+++ src/app/administrador/a-listar-pedidos/a-listar-pedidos.component.ts
@@ -2,67 +2,97 @@ import { Component, OnInit } from '@angular/core';
 import { ProductoService } from 'src/app/servicio/producto.service';
 import { IPedido } from 'src/app/IPedido';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-a-listar-pedidos',
   templateUrl: './a-listar-pedidos.component.html',
-  styleUrls: ['./a-listar-pedidos.component.css']
+  styleUrls: ['./a-listar-pedidos.component.css'],
+  providers: [ DatePipe ]
 })
 export class AListarPedidosComponent implements OnInit {
 
   formPedido: FormGroup;
   constructor( private productService: ProductoService,
+    private datePipe: DatePipe,
     private formBuilder: FormBuilder 
     ) {
       this.formPedido = this.formBuilder.group({
-        nombre:['',[Validators.required]],
-        rut:['',[Validators.required,Validators.maxLength(12)]],
-        telefono:['',[Validators.required,Validators.pattern('[1-9]{1}[0-9]{8}')]],
-        direccion: ['', ],
-        mail:['',[Validators.email]],
-        modo_entrega: ['', [Validators.required]],
-        fecha_inicio: ['',],
+        nombre:[{value: '',disabled:true} , [Validators.required]],
+        rut:[{value: '',disabled:true} , [Validators.required,Validators.maxLength(12)]],
+        telefono:[{value: '',disabled:true} , [Validators.required,Validators.pattern('[1-9]{1}[0-9]{8}')]],
+        direccion: [{value: '',disabled:true} , ],
+        mail:[{value: '',disabled:true} , [Validators.email]],
+        modo_entrega: [{value: '',disabled:true} , [Validators.required]],
+        fecha_inicio: [{value: '',disabled:true} , ],
         fecha_entrega: ['',],
-        fecha_pago: ['',],
-        valor_total: [0,],
-        metodo_pago: ['', [Validators.required]],
-        estado: ['Bolsa de compras',], 
+        fecha_pago: [{value: ''}  ],
+        valor_total: [{value: '',disabled:true} ],
+        metodo_pago: [{value: '',disabled:true} , [Validators.required]],
+        estado: [{value:''}], 
       });
         
     
    }
 
   public pedidos : IPedido[]=[];
+  private id: number;
 
   llenarDatos(pedido: IPedido){
-    this.formPedido.patchValue(pedido);
+
+    this.id=pedido.id;
+
+    this.formPedido.setValue({
+      nombre: pedido.nombre,
+      rut:pedido.rut,
+      mail: pedido.mail,
+      telefono:pedido.telefono,
+      direccion:pedido.direccion,
+      modo_entrega: pedido.modo_entrega,
+      fecha_inicio: this.datePipe.transform(pedido.fecha_inicio,'dd-MM-yyyy'),
+      fecha_entrega: this.datePipe.transform(pedido.fecha_entrega,'dd-MM-yyyy'),
+      fecha_pago: this.datePipe.transform(pedido.fecha_pago,'dd-MM-yyyy'),
+      valor_total: pedido.valor_total,
+      metodo_pago: pedido.metodo_pago,
+      estado: pedido.estado, 
+    });
+    console.log("Llenar DAtos");
+    console.log(this.formPedido.value);
+    console.log(this.formPedido.getRawValue());
+    console.log("raw form");
+
+    console.log(this.formPedido.getRawValue().nombre);
+
+    
   
   }
   ngOnInit() {
     this.productService.getPedidos().subscribe((res: any[]) => {
       this.pedidos = res;
       });
-    /*
-      if(this.pedidos.length){
-        this.formPedido = this.formBuilder.group({
-          nombre:[this.pedidos[0].nombre,[Validators.required]],
-          rut:[this.pedidos[0].rut,[Validators.required,Validators.maxLength(12)]],
-          telefono:[this.pedidos[0].telefono,[Validators.required,Validators.pattern('[1-9]{1}[0-9]{8}')]],
-          direccion: [this.pedidos[0].direccion, ],
-          mail:[this.pedidos[0].mail,[Validators.email]],
-          modo_entrega: [this.pedidos[0].modo_entrega, [Validators.required]],
-          fecha_inicio: [this.pedidos[0].fecha_inicio,],
-          fecha_entrega: [this.pedidos[0].fecha_entrega,],
-          fecha_pago: [this.pedidos[0].fecha_pago,],
-          valor_total: [this.pedidos[0].valor_total,],
-          metodo_pago: [this.pedidos[0].metodo_pago, [Validators.required]],
-          estado: [this.pedidos[0].estado,], 
-        });
-      }
-      */
+  }
+  guardarPedido(pedido: IPedido) {
+
+    /* hace la peticion a la bdd */
+    this.productService.updatePedido(this.id,pedido).subscribe((res: any[]) => {
+      alert('Su pedido ha sido actualizado correctamente')
+      },err => console.log(err));  
   }
   saveData(){
+    //modificar la fecha antes de enviar el pedido...
 
+    console.log("raw form");
+
+    console.log(this.formPedido.getRawValue().nombre);
+
+
+
+    /*
+    this.guardarPedido(this.formPedido.getRawValue());
+    */
+    this.productService.getPedidos().subscribe((res: any[]) => {
+      this.pedidos = res;
+      });
   }
 
 }
